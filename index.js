6efd9c5f25554f4eda4598533f2fce1b23dc1335
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2025-02-08 22:32:50 +0600

    Event create update get APIs create

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send({ mess: 'local server is running' })
+  res.send({ mess: 'local server is running' })
 })
 
 
@@ -28,9 +29,74 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
+    const userCollection = client.db('eventDB').collection('users')
+    const eventCollection = client.db('eventDB').collection('events')
+
+    // user related APIs
+    app.post('/users', async (req, res) => {
+      const data = req.body;
+      const result = await userCollection.insertOne(data);
+      res.send(result)
+    })
+
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result)
+    })
+
+    app.put('/users', async (req, res) => {
+      const data = req.body;
+      const query = { email: data.email }
+      const option = { upsert: true };
+      const updateDoc = {
+        $set: data
+      }
+      const result = await userCollection.updateOne(query, updateDoc, option)
+      res.send(result)
+    })
+
+    // guest login
+    app.post('/guest-login', async (req, res) => {
+      const guestUser = {
+        id: 'guest_' + Date.now(),
+        role: 'guest',
+      };
+      const token = jwt.sign(guestUser, process.env.Secure_Web_Token, { expiresIn: '7h' })
+      res.send({ token })
+    })
+
+    // Events related APIs
+
+    app.post('/events', async (req, res) => {
+      const data = req.body;
+      const result = await eventCollection.insertOne(data);
+      res.send(result)
+    })
+
+    app.get('/events', async (req, res) => {
+      const data = await eventCollection.find().toArray();
+      res.send(data)
+    })
+
+    app.get('/events/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email }
+      const result = await eventCollection.findOne(query);
+      res.send(result)
+    })
+    app.patch('/events/:id', async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: req.body
+
+      }
+      const result = await eventCollection.updateOne(query, updateDoc);
+      res.send(result)
+    })
 
 
-    
 
 
 
@@ -47,5 +113,5 @@ async function run() {
 run().catch(console.dir);
 
 app.listen(port, (req, res) => {
-    console.log(`server is running on port ${port}`)
+  console.log(`server is running on port ${port}`)
 })
